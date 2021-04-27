131c7f8c4f4122e1e9278bc9290120e67265d289
AndreyShashlov <a.shashlov@okpixels.ru>
2021-04-27 16:28:06 +0300

    change max tokens in pool from 8 to 256.

--- contracts/test/BConst.sol
+++ contracts/test/BConst.sol
@@ -7,7 +7,7 @@ contract BConst is BBronze {
     uint public constant BONE              = 10**18;
 
     uint public constant MIN_BOUND_TOKENS  = 2;
-    uint public constant MAX_BOUND_TOKENS  = 8;
+    uint public constant MAX_BOUND_TOKENS  = 256;
 
     uint public constant MIN_FEE           = BONE / 10**6;
     uint public constant MAX_FEE           = BONE / 10;
